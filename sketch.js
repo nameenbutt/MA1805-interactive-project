a920d63cb86b644e09b7dff03490a6a6c7212a9e
nameenbutt <nameenbut20@gmail>
2025-11-28 17:41:50 +0000

    function drawFakeFaceBoxes: glitchBoxes.push = x,y,w,h,life,label

--- sketch.js
+++ sketch.js
@@ -22,6 +22,20 @@ function drawCameraFeed() {
   rect(0, 0, width, height);
 }
 
+function drawFakeFaceBoxes() {
+  if (frameCount % 20 === 0) {
+    glitchBoxes.push ({
+      x: random(width),
+      y: random(height),
+      w: random(80, 160),
+      h: random(80, 160),
+      life: 60,
+      label: random(labels)
+    });
+  }
+
+  
+}
 
 
 
