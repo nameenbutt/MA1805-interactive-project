87d5137e5834d8c20db08ac68a4670902757bf36
nameenbutt <nameenbut20@gmail>
2025-11-28 17:33:26 +0000

    save commands

--- sketch.js
+++ sketch.js
@@ -1,4 +1,10 @@
+let glitchBoxes = [];
+let labels = ["unknown","??","Error","Face?","Access Denied","Loading...","Scanning"];
 
+function setup() {
+  createCanvas (windowWidth, windowHeight);
+  noCursor(); //hide mouse for cool effect
+}
 
 
 
