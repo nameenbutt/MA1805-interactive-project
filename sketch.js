b27b556dbfd077d28c7c776e286e5514dda04486
nameenbutt <nameenbut20@gmail>
2025-11-28 00:53:00 +0000

    save

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
+let eyeImg;
+
+function preload() {
+  eyeImg = loadImage('eyes.jpg'); // Make sure this path is correct
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
   background(220);
+  image(eyeImg, width/2, height/2); // Display the image
 }
+
+
